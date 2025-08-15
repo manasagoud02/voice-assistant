86661d4ce4fc0e6c1408d62ab097936cd64f9824
thanu26-07 <thanujabhupalam@gmail.com>
2025-08-15 18:37:33 +0530

    Update voice-assisstant.py

--- voice-assisstant.py
+++ voice-assisstant.py
@@ -1,1 +1,11 @@
+from PIL import Image
+
+# Take image path from user
+image_path = input("/content/download.jpg")
+
+# Open the image
+img = Image.open(image_path)
+
+# Show the image
+img.show()
 
