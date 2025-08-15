167310a0496222c10d5e539ebc3cf3d3f2bfa948
thanu26-07 <thanujabhupalam@gmail.com>
2025-08-15 18:44:18 +0530

    Update voice-assisstant.py

--- voice-assisstant.py
+++ voice-assisstant.py
@@ -1,11 +1,13 @@
 from PIL import Image
+import matplotlib.pyplot as plt
 
 # Take image path from user
-image_path = input("/content/download.jpg")
+image_path = input("Enter image path: ")
 
 # Open the image
-img = Image.open(image_path)
+img = Image.open("/content/download.jpg")
 
-# Show the image
-img.show()
-
+# Show inline in Colab
+plt.imshow(img)
+plt.axis('off')
+plt.show()
